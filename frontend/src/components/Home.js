7277e824edfa4a9727aa917aace975061dfd2413
Hyungyulee-ant <55528322+tentiti@users.noreply.github.com>
2024-09-21 11:36:29 +0900

    ㅁㅇ

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -293,7 +293,7 @@ function Home() {
             textAlign: 'center',
             color: '#412823',
             marginTop: '20px',
-            marginBottom: '70px'ㄴ
+            marginBottom: '70px'
 
           }}>이제 댄스타임 : 평화의 나무에 달빛이 닿은 날, 반짝이는 춤결<br />
           기획 및 제작 | 유채영 김휴초</span>
